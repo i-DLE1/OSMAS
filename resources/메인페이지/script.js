bbecd9c75f40e4edaf21494796e7d5c8269e5290
KimHaedeun <eun06151@naver.com>
2023-06-12 20:06:59 +0900

    기본

--- resources/메인페이지/script.js
+++ resources/메인페이지/script.js
@@ -1,12 +1,12 @@
 // 헤더 로드
-fetch('../resources/메인페이지/header.html')
+fetch('./resources/메인페이지/header.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('header').innerHTML = data;
   });
 
 // 푸터 로드
-fetch('../resources/메인페이지/footer.html')
+fetch('./resources/메인페이지/footer.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('footer').innerHTML = data;
