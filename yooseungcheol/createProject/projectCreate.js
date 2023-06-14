10bf746c660fc7e85f181cd9012c6ef14d84bd88
yoosc89 <yoosc89@gmail.com>
2023-06-14 22:20:49 +0900

    프로젝트 등록 - 상품, FAQ 컴포넌트 단위 추가 삭제 완료

--- yooseungcheol/createProject/projectCreate.js
+++ yooseungcheol/createProject/projectCreate.js
@@ -1,57 +1,115 @@
-let faqListCount = 1;
-let projectCount = 1;
+let faqListCount = 2;
+let productCount = 2;
 $("#fqa_add").click(function (){
-    faqListCount++;
+    $('#subItem').remove()
+    $(`#faqIndex${faqListCount-1}`).children().children().prop("class","")
     let text = `
-    <div>
-        <label class="subtitle" for="title">제목</label>
-        <input class="inputBox w-100" id="title${faqListCount}" name="title${faqListCount}"/>
-    </div>
-    <div class="mt-4">
-        <label class="subtitle" for="body" >내용</label>
-        <textarea class="inputBox w-100" rows="2" style="resize: none; height: 80px !important;"  id="body${faqListCount}" name="body${faqListCount}"></textarea>
-    </div>
-    <hr class="line">    
-    `
+        <div id="faqIndex${faqListCount}">
+            <div>
+                <div id="faqSubIndex${faqListCount}" class="additem-faq-col2">
+                    <div>
+                        <div>
+                            <label class="subtitle" for="title${faqListCount}">제목</label>
+                            <input class="inputBox w-100" id="title"${faqListCount} name="title${faqListCount}"/>
+                        </div>
+                        <div class="mt-4">
+                            <label class="subtitle" for="body${faqListCount}" >내용</label>
+                            <textarea class="inputBox w-100" rows="2" style="resize: none; height: 80px !important;"  id="body${faqListCount}" name="body"></textarea>
+                        </div>
+                    </div>
+                    <div id="subItem">
+                        <button id="product-sub" class="subItem" onclick="faqItemRemove()">
+                            <img src="/static/image/subItem.png" width="40px" height="40px" >
+                        </button>
+                    </div>
+                </div>
+                <div>
+                    <hr class="line">
+                </div>
+            </div>
+        </div>`
     $('#faq_list').append(text);
+    faqListCount++;
 });
 
-$("#product-add").click(function (){
+function faqItemRemove(){
+    faqListCount--;
+    const elementText =`
+        <div id="subItem">
+            <button id="product-sub" class="subItem" onclick="faqItemRemove()">
+                <img src="/static/image/subItem.png" width="40px" height="40px" >
+            </button>
+        </div>`
+    $(`#faqIndex${faqListCount}`).remove();
+    $(`#faqSubIndex${faqListCount-1}`)
+        .prop("class","additem-faq-col2")
+        .append(elementText)
+}
 
+$("#product-add").click(function (){
+    $("#subItem").remove();
+    $(`#additem-product-index${productCount-1}`).children().prop("class","")
     let text=
-        `<div>
-            <div class="row" style="margin-top: 1em;">
-                <div class="col">
-                    <label class="subtitle" for="name">옵션명</label>
-                    <input class="w-100 inputBox" type="text" id="name${projectCount}" name="name${projectCount}">
+        `<div id="additem-product-index${productCount}">
+            <div id="product-sub-index${productCount}" class="additem-product-col2">
+                <div id="item">
+                    <div class="row" style="margin-top: 1em;">
+                        <div class="col">
+                            <label class="subtitle" for="name">옵션명</label>
+                            <input class="w-100 inputBox" type="text" id="name${productCount}" name="name${productCount}">
+                        </div>
+                        <div class="col-3">
+                            <label for="count" class="subtitle">수량</label>
+                            <input class="w-100 inputBox" type="number" id="count${productCount}" name="count${productCount}">
+                        </div>
+                        <div class="col-3">
+                            <label for="money" class="subtitle">금액</label>
+                            <input class="w-100 inputBox" type="number" id="money${productCount}" name="money${productCount}">
+                        </div>
+                    </div>
+                    <div class="row" style="margin-top: 1em;">
+                        <div class="col">
+                            <label for="body" class="subtitle">설명</label>
+                            <input class="w-100 inputBox" type="text" id="body${productCount}" name="body${productCount}">
+                        </div>
+                        <div class="col-3">
+                            <label for="shippingStart" class="subtitle">배송시작</label>
+                            <input class="w-100 inputBox" type="date" id="shippingStart${productCount}" name="shippingStart${productCount}">
+                        </div>
+                        <div class="col-3">
+                            <label for="shippingFee" class="subtitle">배송비</label>
+                            <input class="w-100 inputBox" type="number" id="shippingFee${productCount}" name="shippingFee${productCount}">
+                        </div>
+                    </div>
                 </div>
-                <div class="col-3">
-                    <label for="count" class="subtitle">수량</label>
-                    <input class="w-100 inputBox" type="number" id="count${projectCount}" name="count${projectCount}">
-                </div>
-                <div class="col-3">
-                    <label for="money" class="subtitle">금액</label>
-                    <input class="w-100 inputBox" type="number" id="money${projectCount}" name="money${projectCount}">
+                <div id="subItem">
+                    <button id="product-sub" class="subItem" onclick="itemRemove()">
+                        <img src="/static/image/subItem.png" width="40px" height="40px" >
+                    </button>
                 </div>
             </div>
-            <div class="row" style="margin-top: 1em;">
-                <div class="col">
-                    <label for="body" class="subtitle">설명</label>
-                    <input class="w-100 inputBox" type="text" id="body${projectCount}" name="body${projectCount}">
-                </div>
-                <div class="col-3">
-                    <label for="shippingStart" class="subtitle">배송시작</label>
-                    <input class="w-100 inputBox" type="date" id="shippingStart${projectCount}" name="shippingStart${projectCount}">
-                </div>
-                <div class="col-3">
-                    <label for="shippingFee" class="subtitle">배송비</label>
-                    <input class="w-100 inputBox" type="number" id="shippingFee${projectCount}" name="shippingFee${projectCount}">
-                </div>
+            <div>
+                <hr class="line">
             </div>
-            <hr class="line">
         </div>`
     $("#product_list").append(text)
+    productCount++;
 })
+
+function itemRemove (){
+    productCount--;
+    let elementText = `
+                <div id="subItem">
+                    <button id="product-sub" class="subItem" onclick="itemRemove()">
+                        <img src="/static/image/subItem.png" width="40px" height="40px" >
+                    </button>
+                </div>`
+    $(`#additem-product-index${productCount}`).remove();
+    $(`#product-sub-index${productCount-1}`)
+        .append(elementText)
+        .prop("class","additem-product-col2");
+
+}
 
 function productList(){
     // 더미 데이터
